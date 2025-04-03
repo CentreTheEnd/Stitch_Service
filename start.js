4f9ccc7cb4358c56a3a709338c23157812c97a2b
Naruto - 2000 <ssawaza30@gmail.com>
2025-04-03 13:16:30 +0200

    Update start.js

--- start.js
+++ start.js
@@ -1,7 +1,20 @@
 import { setupApp } from './main.js';
 
-console.log(`Loading Start Project...`)
+console.log(`Loading Start Project...`);
 
+try {
+    const PORT = process.env.PORT || 3000;
+    const app = await setupApp();
+    console.log(`Loading Start in port:`, PORT);
+    
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+} catch (error) {
+    console.error("Error while starting the server:", error);
+    process.exit(1);
+}
+
+/*
 setupApp()
 .then(app => {
     const PORT = process.env.PORT || 3000;
@@ -11,3 +24,4 @@ setupApp()
     console.error("Error while starting the server:", error);
         process.exit(1);
     });
+*/
