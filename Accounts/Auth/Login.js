c1d3b42c27358c8d3bd2c24d5a2bf49f8a0605cf
Naruto - 2000 <ssawaza30@gmail.com>
2025-04-14 21:02:35 +0200

    Update Login.js

--- Accounts/Auth/Login.js
+++ Accounts/Auth/Login.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import bcrypt from 'bcryptjs';
-import User from '../../model.js';
+import { User } from '../../Database/Mongo/models.js';
 
 const router = express.Router();
 
