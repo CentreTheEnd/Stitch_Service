e49a1459b956381849604ff585bccd78313310da
Naruto - 2000 <ssawaza30@gmail.com>
2025-04-15 12:26:31 +0200

    Update start.js

--- start.js
+++ start.js
@@ -7,50 +7,6 @@ try {
     const app = await setupApp();
     console.log(`Loading Start in port:`, PORT);
     
-    const redirectToError = (res, code) => {
-  if (!res.headersSent) {
-    res.status(code).redirect(`/html/error.html?code=${code}`);
-  }
-};
-
-  app.use((err, req, res) => {
-  console.error('خطأ في الخادم:', err.stack);
-
-  let errorCode = 500;
-
-  if (err.name === 'MongoError') {
-    errorCode = 500;
-  }
-
-  else if (err.status === 404) {
-    errorCode = 404; // Not Found
-  }
-
-  else if (err.status === 401) {
-    errorCode = 401; // Unauthorized
-  }
-
-  else if (err.status === 403) {
-    errorCode = 403; 
-  }
-
-  else if (err.code === 'ETIMEOUT') {
-    errorCode = 504; 
-  }
-
-  else if (err.name === 'Error') {
-    errorCode = 500; 
-  }
-
-  else if (err.status === 400) {
-    errorCode = 400; 
-  }
-
- 
-  redirectToError(res, errorCode);
-});
-    
-    
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 } catch (error) {
