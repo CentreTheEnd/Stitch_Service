e7b29e923680b2e4a4afeb0e1a41600d4a6ec7bb
Naruto - 2000 <ssawaza30@gmail.com>
2025-04-17 23:09:25 +0200

    Update index.js

--- Switch/index.js
+++ Switch/index.js
@@ -163,7 +163,7 @@ export async function setupRoutes(app) {
     if (!req.originalUrl.toLowerCase().includes('/api/v2') || !req.originalUrl.toLowerCase().startsWith('/api/v2')) return next();
         
     const apiKeyHeader = req.headers['api-key'];
-    if (!apiKeyHeader && apiKeyHeader !== global.isApiKey) {
+    if (!apiKeyHeader || apiKeyHeader !== global.isApiKey) {
         return res.status(400).json({ status: false, message: 'Missing API Key' });
     }
 
