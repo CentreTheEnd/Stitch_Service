bd781a9a536ad201aef9cd7400153cf84e2354d5
Naruto - 2000 <ssawaza30@gmail.com>
2025-04-17 22:04:02 +0200

    Update futchions.js

--- futchions.js
+++ futchions.js
@@ -1,26 +1,31 @@
+import crypto from 'crypto';
 
-const generateToken = () => {
-    return crypto.randomBytes(32).toString('hex'); 
+global.createdKeys = {
+  firstKey: "stitch",
+  endKey: "stitch_no_hacking_here"
 };
 
-const generateCookies = () => {
-    const sessionId = crypto.randomBytes(16).toString("hex");
-    return `sessionId=${sessionId}; HttpOnly; Secure; SameSite=Strict; Path=/`;
+global.generateToken = () => {
+  return crypto.randomBytes(32).toString('hex'); 
 };
 
-const hashPassword = (password) => {
-    return crypto.createHash('sha256').update(password).digest('hex');
+global.generateCookies = () => {
+  const sessionId = crypto.randomBytes(16).toString("hex");
+  return `sessionId=${sessionId}; HttpOnly; Secure; SameSite=Strict; Path=/`;
 };
 
-const md5Reverse = (inputString) => {
-    let md5Hash = crypto.createHash("md5").update(inputString, "utf-8").digest("hex");
-    return md5Hash.split("").reverse().join("");
+global.hashPassword = (password) => {
+  return crypto.createHash('sha256').update(password).digest('hex');
 };
 
-const generateAPIKey = (user) => {
-    const randomNumbers = crypto.randomInt(10000, 99999);
-    const randomHex = crypto.randomBytes(6).toString("hex");
-    const encryptedKey = md5Reverse(user + randomHex + global.createdKeys.endKey);
+global.md5Reverse = (inputString) => {
+  let md5Hash = crypto.createHash("md5").update(inputString, "utf-8").digest("hex");
+  return md5Hash.split("").reverse().join("");
+};
 
-    return `${global.createdKeys.firstKey}-${randomNumbers}-${encryptedKey}`;
+global.generateAPIKey = (ipOrString = "default") => {
+  const randomNumbers = crypto.randomInt(10000, 99999);
+  const randomHex = crypto.randomBytes(6).toString("hex");
+  const encryptedKey = global.md5Reverse(ipOrString + randomHex + global.createdKeys.endKey);
+  return `${global.createdKeys.firstKey}-${randomNumbers}-${encryptedKey}`;
 };
