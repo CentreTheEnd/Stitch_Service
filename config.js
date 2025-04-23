07fb393a12a00e9607e43508cd367ce1b25ba575
Sayed Shawaza <ssawaza30@gmail.com>
2025-04-23 17:52:15 +0200

    Update config.js

--- config.js
+++ config.js
@@ -16,47 +16,10 @@ import moment from 'moment-timezone';
 import dotenv from "dotenv";
 import './futchions.js';
 dotenv.config();
-//______________________________________________
-
-global.author = '⛊  𝚂𝙰𝚈𝙴𝙳-𝚂𝙷𝙰𝚆𝙰𝚉𝙰';
-
-
 
 //______________________________________________
 
-
-/*
-const config = {
-  owner: {
-    name: "Sayed Hamdey",
-    email: "sayeddaana221166@gmail.com"
-  },
-  github: {
-    repoOwner: "sayed-hamdey-2000"
-  },
-  setting: {
-    githubToken: "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",
-    driveKey: "AIzaSyC9AL_PCz0Zq6jEtv9iVJg9-fIUymgn9wg",
-    mongoUrl: "mongodb+srv://shawaza:Ss24-4-2004@cluster0.kz7o9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-  },
-  auth: {
-    google: {
-      id: "687788561377-1emm0v529c1kfn8p7d1tsvt5eku4ks2e.apps.googleusercontent.com",
-      secret: "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"
-    },
-    github: {
-      id: "Ov23life6Ac2whmERyvW",
-      secret: "780bee44b86bdafbf7f319396a83487e0aac5568"
-    },
-    discord: {
-      id: "1355288606268199013",
-      secret: "48da5dd2ac07c189be1d98c48f255e66e160067ed345e9558fb3ed456babc736",
-      oauth2: "https://discord.com/oauth2/authorize?client_id=1355288606268199013"
-    }
-  }
-};
-
-*/
+//______________________________________________
 
 //______________________________________________
 
@@ -112,9 +75,6 @@ global.githubSecret = config.auth.github.secret;
 global.discordID = config.auth.discord.id;
 global.discordSecret = config.auth.discord.secret;
 global.discordUrl = config.auth.discord.oauth2;
-
-
-
 
 //______________________________________________
 
