a0ac42a50d5f0a82f9ce2a918536f39059a4ac29
Sayed Shawaza <ssawaza30@gmail.com>
2025-04-23 20:32:42 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,9 +3,7 @@ import cors from "cors";
 import express from 'express';
 import bodyParser from 'body-parser';
 import path from 'path';
-//import { Low, JSONFile } from 'lowdb';
 import { fileURLToPath } from 'url';
-//import fs from "fs/promises";
 import fs from 'fs';
 
 
@@ -13,8 +11,7 @@ import fs from 'fs';
 //______________________________________________
 
 import './config.js';
-//import { setupRoutes } from './Switch/Api/index.js';
-//import { setupDatabase } from './Switch/Accounts/index.js';
+
 import { connectDB } from './Database/Mongo/connect.js';
 
 import { setupRoutes } from './Switch/index.js';
@@ -28,49 +25,13 @@ const dbFilePath = path.join(__dirname, 'Database', 'Storage', 'database.json');
 
 //______________________________________________
 
-// global.db = new Low(new JSONFile(dbFilePath));
 
 //______________________________________________
 
-/* 
-global.loadDatabase = async function loadDatabase() {
-  if (global.db.READ) {
-    return new Promise((resolve) => setInterval(async function () {
-      if (!global.db.READ) {
-        clearInterval(this);
-        resolve(global.db.data == null ? global.loadDatabase() : global.db.data);
-      }
-    }, 1 * 1000));
-  }
-
-  if (global.db.data !== null) return;
-  global.db.READ = true;
-  await global.db.read().catch(console.error);
-  global.db.READ = null;
-
+export async function setupBot() {
   
-  global.db.data = {
-    users: {},
-    posties: {},
-    ...(global.db.data || {}),
-  };
 
-  
-  global.db.data = new Proxy(global.db.data, {
-    set: async function (target, prop, value) {
-      target[prop] = value;
-   
-      await global.db.write();
-      console.log(`Data saved after change to property: ${prop}`);
-      return true;
-    }
-  });
-    
-  await global.db.write();
-};
-*/
-
-
+}
 //______________________________________________
 
 export async function setupApp() {
@@ -88,9 +49,6 @@ export async function setupApp() {
 
     console.log(`oInitializing Express App...`);
 
-  //  console.log(`Setting up Database...`);
-  //  await loadDatabase();
-
     const app = express();
   
     let html;
@@ -101,35 +59,11 @@ export async function setupApp() {
     app.use(cors());
     app.use(express.json());
     app.use(bodyParser.json());
-   // app.use(express.static("public"));
     app.use(express.static(path.join(__dirname, 'public')));
     app.use(express.urlencoded({ extended: true }));
 
     console.log(`Setting up Routes...`);
     await setupRoutes(app);
-
-    //console.log(`Setting up API Accounts...`);
-    //await setupDatabase(app);
-    
-/*
-    app.use(async (req, res, next) => {
-        const authHeader = req.headers['authorization'];
-        
-        if (authHeader) {
-            const apiKey = authHeader.split(' ')[1];
-            next();
-        } else {
-             return res.status(401).json({
-             success: false,
-             message: 'API key missing or malformed',
-             });
-        }
-    });
-
-*/
-    
-    //console.log(`Setting up API Routes...`);
-    //await setupRoutes(app);
 
     console.log(`Server Setup Complete!`);
 
@@ -144,15 +78,6 @@ app.get('/', (req, res) => {
          }
     });
   
-  /*
-    app.get('/downloader/video', (req, res) => {
-        const ip = req.ip;
-        const apiKey = global.generateAPIKey(ip);
-      
-         res.setHeader('api-key', apiKey);
-         res.sendFile(path.join(__dirname,`/public/html/downloader_video.html`));
-    });
-    */
   app.get('/downloader/video', (req, res) => {
   const ip = req.ip;
   apiKey = global.generateAPIKey(ip);
@@ -224,6 +149,7 @@ app.get('/', (req, res) => {
 
     return app;
 } 
+
 //______________________________________________
    
 //______________________________________________
