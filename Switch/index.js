3bfbcc3bd538847f1c940ec0e55c5cd7234eb81c
Sayed Shawaza <ssawaza30@gmail.com>
2025-04-30 01:08:27 +0300

    Update index.js

--- Switch/index.js
+++ Switch/index.js
@@ -167,9 +167,13 @@ export async function setupRoutes(app) {
     if (!req.originalUrl.toLowerCase().includes('/api/v2') || !req.originalUrl.toLowerCase().startsWith('/api/v2')) return next();
         
     const apiKeyHeader = req.headers['api-key'];
-   /* if (!apiKeyHeader) { // || apiKeyHeader !== global.isApiKey
+    const verKey req.headers['x-vercel-dev'];
+         
+    const verCo req.headers['dev-co'];
+        
+    if (!verCo || !verKey || !apiKeyHeader || apiKeyHeader !== global.isApiKey) { // || apiKeyHeader !== global.isApiKey
         return res.status(400).json({ status: false, message: 'Missing API Key' });
-    } */
+    } 
 
         /*
 
