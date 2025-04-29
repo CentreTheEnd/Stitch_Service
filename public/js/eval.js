69907a40b2498faad53a612497a0b138bf7dc5e8
Sayed Shawaza <ssawaza30@gmail.com>
2025-04-29 09:54:55 +0300

    Create eval.js

---
+++ public/js/eval.js
@@ -0,0 +1,21 @@
+function runCode() {
+  const code = document.getElementById('code').value;
+  const resultElement = document.getElementById('result');
+
+  fetch('/eval', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ code })
+  })
+  .then(res => res.json())
+  .then(data => {
+    if (data.success) {
+      resultElement.textContent = `Result:\n${data.result}`;
+    } else {
+      resultElement.textContent = `Error:\n${data.error}`;
+    }
+  })
+  .catch(err => {
+    resultElement.textContent = `Fetch Error:\n${err.message}`;
+  });
+}
