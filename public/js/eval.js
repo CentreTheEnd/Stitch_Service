83ade138aee316d065d977879c06b983c8a2529d
Sayed Shawaza <ssawaza30@gmail.com>
2025-04-29 09:57:17 +0300

    Update eval.js

--- public/js/eval.js
+++ public/js/eval.js
@@ -2,7 +2,7 @@ function runCode() {
   const code = document.getElementById('code').value;
   const resultElement = document.getElementById('result');
 
-  fetch('/eval', {
+  fetch('/api/v3/eval', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ code })
