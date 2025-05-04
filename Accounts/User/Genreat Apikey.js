a11476d8858ac4c7f7d672e56cd4bb72a52e7413
Sayed Shawaza <ssawaza30@gmail.com>
2025-05-04 16:27:36 +0300

    Update Genreat Apikey.js

--- Accounts/User/Genreat Apikey.js
+++ Accounts/User/Genreat Apikey.js
@@ -9,10 +9,13 @@ router.get('/genreat/apikey', async (req, res) => {
   try {
     
 const apikey = await global.db.genrate.APIKey("sayeddaana221166@gmail.com", "key");
+const apikeys = [...global.db.generatedAPIKeys];
+    
   
     res.status(200).json({
       success: true,
-      apikey: apikey
+      apikey: apikey,
+      all: apikeys
     });
   } catch (err) {
     res.status(500).json({ success: false, error: err.message });
